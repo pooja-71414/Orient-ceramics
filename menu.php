80f08077eaa9609cadb018cb5523d51fbd8e9ec2
pooja-71414 <pooja71414@gmail.com>
2023-12-29 15:23:10 +0530

    menu

--- menu.php
+++ menu.php
@@ -1,18 +1,12 @@
 <?php include 'css.php'; ?>
-<nav class="navbar navbar-expand-lg bg bg-primary navbar light">
+
+<nav class="navbar navbar-expand-lg bg bg-primary navbar light mt-2">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">NAVBAR</a>
-    <span class="navbar-toggler-icon"></span>
-        <div class="collapse navbar-collapse" id="navbar">
-           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">
-                        WEL-COME
-                    </a>
-                </li>
-           </ul> 
-        </div>
+    <a class="navbar-brand">NAVBAR</a>
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
   </div>
 </nav>
-<?php include 'js.php'?>
-
+<?php include 'js.php' ?>
