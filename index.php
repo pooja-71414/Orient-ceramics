53df351a66a4615b74360fb91b37af97ac986bcf
pooja-71414 <pooja71414@gmail.com>
2023-12-29 15:34:36 +0530

    change

--- index.php
+++ index.php
@@ -12,35 +12,36 @@
 
     <div class="container-fluid mt-4">
         
-        <div class="card w-50 m-auto">
+        <div class="card w-75 m-auto bg bg-light">
             <div class="card-header">
-                <h3 class="text text-warning text-center">Employee Table</h3>
+                <h3 class="text text-dark
+ text-center">Employee Table</h3>
             </div>
             <div class="card-body">
                 <form class="form" enctype="multipart/form-data">
                     <div class="form-group">
                         <label class="form-label" for="e_id">* Employee Id</label>
-                        <input type="number" class="form-control" name="e_id" placeholder="Entere Id Here" required/>
+                        <input type="number" class="form-control" name="e_id" placeholder="Enter Id Here" required/>
                     </div>
                     <div class="form-group">
                         <label class="form-label" for="ename">* Employee Name</label>
-                        <input type="text" class="form-control" name="ename" placeholder="Entere Your Name Here"/>
+                        <input type="text" class="form-control" name="ename" placeholder="Enter Your Name Here" required/>
                     </div>
                     <div class="form-group">
                         <label class="form-label" for="ecity">* Employee City</label>
-                        <input type="text" class="form-control" name="ecity" placeholder="Entere City Name Here"/>
+                        <input type="text" class="form-control" name="ecity" placeholder="Enter City Name Here" required/>
                     </div>
                     <div class="form-group">
                         <label class="form-label" for="econtact">* Employee Contact Number</label>
-                        <input type="number" class="form-control" name="econtact"/>
+                        <input type="number" class="form-control" name="econtact" placeholder="Enter Your Contact Number Here" required/>
                     </div>
                     <div class="form-group">
                         <label class="form-label" for="esalary">* Employee Salary</label>
-                        <input type="number" class="form-control" name="esalary"/>
+                        <input type="number" class="form-control" name="esalary" placeholder="Enter Your Salary Here" required/>
                     </div>
                     <div class="form-group">
                         <label class="form-label" for="esign">* Employee Signature</label>
-                        <input type="file" class="form-control" name="esign"/>
+                        <input type="file" class="form-control" name="esign" required/>
                     </div>
                     <div class="mt-3">
                         <button class="btn btn-danger" type="submit" name="submit">Submit</button>
