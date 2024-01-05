523e19d829843870fb8f4d098aac03d6407e59cb
pooja-71414 <pooja71414@gmail.com>
2024-01-05 15:39:08 +0530

    responsive_table

--- display.php
+++ display.php
@@ -25,12 +25,10 @@ require_once('main.php');
                 </h3>
             </div>
             <div class="card card-body">
-
                 <?php
                 //$id=$_GET['e_id'];
                 $data = $ob->view();
                 while ($row = mysqli_fetch_assoc($data)) {
-
                 ?>
                     <div class="row ">
                         <div class="col">
@@ -39,28 +37,64 @@ require_once('main.php');
                         <div class="col">
                             <?php echo $row["ename"] ?>
                         </div>
-                        <div class="col"> 
+                        <div class="col">
                             <form action="" method="POST">
-                            <a href="view.php?id=<?php echo $row['e_id']; ?>" class="btn btn-info">
-                                view
-                            </a>
-                            
-                        <a href="update.php?id=<?php echo $row['e_id']; ?>" class="btn btn-success">update</a>
-                            
-                          
-                                <input type="number" name="e_id" value="<?php echo $row['e_id'];
-                                                                        ?>" hidden>
+                                <a href="view.php?id=<?php echo $row['e_id']; ?>" class="btn btn-info">
+                                    view
+                                </a>
+                                <a href="update.php?id=<?php echo $row['e_id']; ?>" class="btn btn-success">update</a>
+                                <input type="number" name="e_id" value="<?php echo $row['e_id']; ?>" hidden>
                                 <button class="btn btn-danger" type="submit" name="delete" onclick="return confirm('are you sure to delete')">delete</button>
                             </form>
                         </div>
-                        
-
                     </div>
                     <hr class="bg bg-info">
                 <?php } ?>
             </div>
         </div>
     </div>
+
+
+    <!--<div class="table-responsive">
+            <table class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><?php //echo $row["e_id"]; 
+                            ?></td>
+                        <td><?php //echo $row["ename"]; 
+                            ?></td>
+                        <td>                        
+                            <form action="" method="POST">
+                                <a href="view.php?id=<?php //echo $row['e_id']; 
+                                                        ?>" class="btn btn-info">
+                                    view
+                                </a>
+
+                                <a href="update.php?id=<?php //echo $row['e_id']; 
+                                                        ?>" class="btn btn-success">update</a>
+
+
+                                <input type="number" name="e_id" value="<?php //echo $row['e_id'];
+                                                                        ?>" hidden>
+                                <button class="btn btn-danger" type="submit" name="delete" onclick="return confirm('are you sure to delete')">delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                    <?php
+                    //}
+                    ?>
+            </tbody>
+            </table>
+        </div>-->
+                    
+
     <?php include 'js.php' ?>
 </body>
 
